631243ce649ef1d1c4d84c32083e7dbf6706ebdc
Bennie Mosher <bmosher@bmosher-995.rii.io>
2023-04-04 12:04:38 -0400

    Created zshell Ansible role

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/usr/local/bin/brew shellenv)"
+
+# Install brew software
+brew bundle
+
+# Run ansible tasks
+ansible-playbook playbook.yml
