dfa66e1b1b39890822978c9bf82da23b30e53607
Bennie Mosher <bmosher@researchinnovations.com>
2023-10-23 10:11:41 -0600

    Checking in font URL changes

--- install.sh
+++ install.sh
@@ -2,9 +2,7 @@
 
 # Install Homebrew
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/bmosher/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/usr/local/bin/brew shellenv)"
 
 # Install brew software
 brew bundle
