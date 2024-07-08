dc6dfd4a4f08e02519ed10e2fab6d6676d40282d
Bennie Mosher <benniemosher@gmail.com>
2024-07-08 10:08:08 -0600

    Updated to run cleanly

---
+++ README.md
@@ -0,0 +1,9 @@
+# dotfiles
+
+New rendition utilizing Ansible for configuration.
+
+To install or update:
+
+```bash
+./install.sh
+```
--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
 #!/usr/bin/env bash
 
 # Install Homebrew
-
-# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/usr/local/bin/brew shellenv)"
+if command -v brew >&2; then
+  echo Homebrew is already installed. Skipping install.
+else
+  echo Installing Homebrew...
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
 
 # Install brew software
+echo Installing Homebrew packages
 brew bundle
 
-# Run ansible tasks
-ansible-playbook playbook.yml
+# Install ansible
+if command -v ansible >&2; then
+  echo Ansible is already installed. Skipping install.
+else
+  echo Installing Ansible...
+  pipx install --include-deps ansible
+  pipx ensurepath
+fi
 
-gh auth login
+# Run ansible tasks
+if command -v ansible >&2; then
+  echo Running Ansible playbook
+  ansible-playbook playbook.yml --ask-become-pass
+else
+  echo Cannot find Ansible.
+fi
+
+if ! gh auth status >/dev/null 2>&1; then
+  gh auth login
+fi
